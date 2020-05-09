f81064f2b90ea6e13568df0a00072d6a9c4f41c7
Arturo <arturoescamilla@outlook.com>
2020-05-09 13:22:25 -0500

    update generis types

---
+++ Assets/Scripts/GenerycTypes/GenericMethodExample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericMethodExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Evaluate("Hello world");
+        Evaluate(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Evaluate<T>(T suppliedValue)
+    {
+        Debug.LogFormat("The type of {0} is {1}", suppliedValue, typeof(T));
+    }
+}
