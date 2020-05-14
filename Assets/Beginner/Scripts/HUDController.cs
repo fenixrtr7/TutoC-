1d6c32fac919e5e93eb90f3209d5952ca53dfb24
Arturo <arturoescamilla@outlook.com>
2020-05-14 11:15:07 -0500

    update Course

---
+++ Assets/Beginner/Scripts/HUDController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public delegate void TextOutputHandler(string text);
+
+public class HUDController : MonoBehaviour
+{
+    public Text textPause, textScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OutPutText(string output)
+    {
+        Debug.LogFormat("{0} output by GameSceneController", output);
+    }
+}
---
+++ Assets/TutoSuv/Scripts/Delegates/ButonAction/ExecuteButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExecuteButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Main5.onClick += Test;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Test()
+    {
+        Debug.Log("Esto es un test");
+    }
+}
